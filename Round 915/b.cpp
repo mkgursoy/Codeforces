833c7cd5ab393766a773042e96f39aa803ce9c1d
Metin Kasım Gürsoy <145182046+mkgursoy@users.noreply.github.com>
2023-12-16 20:51:23 +0300

    Add files via upload

---
+++ Round 915/b.cpp
@@ -0,0 +1,78 @@
+/*
+    Author:Matkap(prefixsumenjoyer23)
+    
+*/
+
+#include <bits/stdc++.h>
+#include <array>
+
+using namespace std;
+
+#define fr freopen("/Users/metinkasim/Documents/in.txt","r",stdin);freopen("/Users/metinkasim/Documents/out.txt","w",stdout);
+#define int long long
+#define endl "\n"
+#define ar array
+#define all(x) x.begin(),x.end()
+
+int INF = 1e18;
+
+void setIO(string name = "") 
+{
+	if (name.size())
+	{
+		freopen((name + ".in").c_str(), "r", stdin); // see /general/input-output
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+int binpow(int base,int power)
+{
+	if(power == 1) return base;
+	if(power == 0) return 1;
+    
+     if(power%2==1)
+     {
+     	 int a;
+     	a = binpow(base,(power-1)/2);
+     	return a*a*base;
+     } 
+     else
+     {
+     	 int a;
+     	a = binpow(base,power/2);
+     	return a*a;
+     } 
+
+}
+void solve()
+{
+   	map<int,int> mp;
+   	int mx = 0;
+   	int n;
+   	cin >> n;
+   	for(int i = 0;n-1 > i;i++)
+   	{
+   		int x,y;
+   		cin >> x >> y;
+   		mp[x]++;
+   		mp[y]++;
+   	}
+   	int sum = 0;
+   	for(auto it : mp) sum += max(0ll,(it.second-2));
+   	cout << (sum+1)/2 + 1 << endl;
+}
+
+
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	//fr
+
+	int t;
+	t=1;
+	cin >> t;
+	while(t--) solve();
+
+}
